07a2f545ca0e594025568cb02e8ee952e4e14363
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-06-09 09:37:04 +0200

    Create maintenance_plan module

---
+++ maintenance_plan/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{'name': 'Maintenance Plan',
+ 'version': '10.0.1.0.0',
+ 'author': 'Odoo Community Association (OCA), Camptocamp SA',
+ 'license': 'AGPL-3',
+ 'category': 'Maintenance',
+ 'website': 'http://www.camptocamp.com',
+ 'images': [],
+ 'depends': [
+     'maintenance',
+     ],
+ 'data': [
+     'security/ir.model.access.csv',
+     'views/maintenance.xml'
+     ],
+ 'demo': [
+     'data/demo_maintenance_plan.xml'
+ ],
+ 'installable': True,
+ 'auto_install': False,
+ }
