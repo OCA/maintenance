eed3a9b5689c576fc17c18f8360fd722e0accad1
Matteo Mazzoni <matteo@bestmazzo.it>
2018-10-25 23:49:01 +0200

    [MIG] maintenance_plan: Migration to 11.0

--- maintenance_plan/__manifest__.py
+++ maintenance_plan/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Maintenance Plan',
  'summary': 'Extends preventive maintenance planning',
- 'version': '10.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'author': 'Odoo Community Association (OCA), Camptocamp SA',
  'license': 'AGPL-3',
  'category': 'Maintenance',
- 'website': 'http://www.camptocamp.com',
+ 'website': 'https://github.com/OCA/maintenance',
  'images': [],
  'depends': [
      'maintenance',
@@ -21,5 +20,4 @@
  ],
  'post_init_hook': 'post_init_hook',
  'installable': True,
- 'auto_install': False,
  }
