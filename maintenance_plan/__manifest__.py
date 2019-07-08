851963b1a9f5df8f6fb0d4e0ef6510767343589f
David Alonso (Solvos) <david.alonso@solvos.es>
2019-07-08 11:46:06 +0200

    [MIG] maintenance_plan: Migration to 12.0

--- maintenance_plan/__manifest__.py
+++ maintenance_plan/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Maintenance Plan',
  'summary': 'Extends preventive maintenance planning',
- 'version': '11.0.1.0.0',
+ 'version': '12.0.1.0.0',
  'author': 'Odoo Community Association (OCA), Camptocamp SA',
  'license': 'AGPL-3',
  'category': 'Maintenance',
