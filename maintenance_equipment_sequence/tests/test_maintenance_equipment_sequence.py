65da3daeadee3d891f96ad526c71936a0a2b8804
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-11-06 12:52:53 +0100

    fixup and increase test coverage

--- maintenance_equipment_sequence/tests/test_maintenance_equipment_sequence.py
+++ maintenance_equipment_sequence/tests/test_maintenance_equipment_sequence.py
@@ -30,11 +30,32 @@ class TestMaintenanceEquipmentSequence(TransactionCase):
         self.assertEqual(seq_1.number_next_actual,
                          categ_1.sequence_number_next)
 
+        # Create category without sequence, then write prefix and number next
+        categ_2 = self.maintenance_equipment_categ_obj.create({
+            'name': 'Test Category 2',
+        })
+        categ_2.write({
+            'sequence_prefix': 'TTC2',
+            'sequence_number_next': 100,
+        })
+        seq_2 = self.sequence_obj.search(
+            [('name', '=', categ_2.name), ('prefix', '=', 'TTC2')], limit=1)
+        self.assertEqual(seq_2.prefix, categ_2.sequence_prefix)
+        self.assertEqual(seq_2.number_next_actual,
+                         categ_2.sequence_number_next)
+
+        # Assign sequence 1 to category 2
+        categ_2.write({
+            'sequence_id': seq_1.id,
+        })
+        categ_2._onchange_sequence_id()
+
         # Create equipment inside category
         equipment_1 = self.maintenance_equipment_obj.create({
             'name': 'Laptop 1',
-            'category_id': categ_1.id,
+            'category_id': categ_2.id,
         })
+        categ_2._compute_equipment_code()
         self.assertEqual(equipment_1.code, 'TTC0001')
         # Set code manually
         equipment_1.write({'code': 'TTC0023'})
