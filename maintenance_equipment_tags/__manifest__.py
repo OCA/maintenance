44015441d5b9f2f953879681b4229031a0b4a77f
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-11-18 16:48:19 +0100

    [11.0][FIX] maintenance_equipment_tags: search view
    maintenance_equipment_tags 11.0.1.0.1

--- maintenance_equipment_tags/__manifest__.py
+++ maintenance_equipment_tags/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Maintenance Equipment Tags',
     'summary': """
         Adds category tags to equipment""",
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'license': 'AGPL-3',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/maintenance',
