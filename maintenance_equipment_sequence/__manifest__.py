43629121ab036908bd36667938330ce850254890
Núria Martín Xifré <nuria.martin@eficent.com>
2019-12-04 14:02:13 +0100

    [12.0][MIG]maintenance_equipment_sequence

--- maintenance_equipment_sequence/__manifest__.py
+++ maintenance_equipment_sequence/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': """
         Adds sequence to maintenance equipment defined in the equipment's
         category""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Eficent Business and IT Consulting Services S.L.,'
               'Odoo Community Association (OCA)',
