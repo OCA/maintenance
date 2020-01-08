c83ead3086191d53d2a73faf7390cd11bcbc1d47
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-01-08 17:43:40 +0100

    [12.0][MIG] maintenance_request_stage_transition

--- maintenance_request_stage_transition/__manifest__.py
+++ maintenance_request_stage_transition/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Maintenance Request Stage transition',
     'summary': """
         Manage transition visibility and management between stages""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/maintenance',
