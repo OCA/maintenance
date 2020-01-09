73c48b4d3a7fd42155e2e09ddb932109b09f69d7
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-09 09:31:45 +0100

    [12.0][MIG] maintenance_equipment_hierarchy
    [UPD] Update maintenance_equipment_hierarchy.pot

--- maintenance_equipment_hierarchy/__manifest__.py
+++ maintenance_equipment_hierarchy/__manifest__.py
@@ -1,21 +1,15 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2020 ForgeFlow S.L. (https://forgeflow.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Maintenance Equipments Hierarchy',
-    'summary': 'Manage equipment hierarchy',
-    'author': 'Eficent, Odoo Community Association (OCA)',
-    'website': 'http://github.com/OCA/maintenance',
-    'category': 'Equipments, Assets, Internal Hardware, Allocation Tracking',
-    'version': '11.0.1.1.0',
-    'license': 'LGPL-3',
-    'depends': [
-        'maintenance',
-    ],
-    'data': [
-        'views/maintenance_equipment_views.xml',
-    ],
-    'demo': [
-        'data/demo_maintenance_equipment_hierarchy.xml'
-    ],
+    "name": "Maintenance Equipments Hierarchy",
+    "summary": "Manage equipment hierarchy",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "http://github.com/OCA/maintenance",
+    "category": "Equipments, Assets, Internal Hardware, Allocation Tracking",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["maintenance"],
+    "data": ["views/maintenance_equipment_views.xml"],
+    "demo": ["data/demo_maintenance_equipment_hierarchy.xml"],
 }
