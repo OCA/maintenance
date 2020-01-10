388ef9107335ae9bf1d0771fe75ae268156787c4
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-10 10:20:38 +0100

    [IMP] maintenance_equipment_sequence: black, isort

--- maintenance_equipment_sequence/__manifest__.py
+++ maintenance_equipment_sequence/__manifest__.py
@@ -2,19 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Maintenance Equipment Sequence',
-    'summary': """
+    "name": "Maintenance Equipment Sequence",
+    "summary": """
         Adds sequence to maintenance equipment defined in the equipment's
         category""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Eficent Business and IT Consulting Services S.L.,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.github.com/OCA/maintenance',
-    'depends': [
-        'maintenance'
-    ],
-    'data': [
-        'views/maintenance_views.xml',
-    ],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Eficent Business and IT Consulting Services S.L.,"
+    "Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/maintenance",
+    "depends": ["maintenance"],
+    "data": ["views/maintenance_views.xml"],
 }
