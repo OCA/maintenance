f759cc2636026283bc00ca853cac143fcb214474
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-14 09:51:53 +0100

    [IMP] maintenance_plan: black, isort

--- maintenance_plan/__manifest__.py
+++ maintenance_plan/__manifest__.py
@@ -1,25 +1,22 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-{'name': 'Maintenance Plan',
- 'summary': 'Extends preventive maintenance planning',
- 'version': '12.0.3.0.0',
- 'author': 'Camptocamp SA, Eficent, Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'category': 'Maintenance',
- 'website': 'https://github.com/OCA/maintenance',
- 'images': [],
- 'depends': [
-     'maintenance',
-     ],
- 'data': [
-     'security/ir.model.access.csv',
-     'views/maintenance_kind_views.xml',
-     'views/maintenance_plan_views.xml',
-     'views/maintenance_equipment_views.xml',
-     ],
- 'demo': [
-     'data/demo_maintenance_plan.xml'
- ],
- 'post_init_hook': 'post_init_hook',
- 'installable': True,
- }
+{
+    "name": "Maintenance Plan",
+    "summary": "Extends preventive maintenance planning",
+    "version": "12.0.3.0.0",
+    "author": "Camptocamp SA, Eficent, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Maintenance",
+    "website": "https://github.com/OCA/maintenance",
+    "images": [],
+    "depends": ["maintenance"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/maintenance_kind_views.xml",
+        "views/maintenance_plan_views.xml",
+        "views/maintenance_equipment_views.xml",
+    ],
+    "demo": ["data/demo_maintenance_plan.xml"],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+}
