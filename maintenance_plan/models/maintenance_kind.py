d80b924986904cb76ce7428018a2c569ed9bc6af
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-14 09:54:47 +0100

    [13.0][MIG] maintenance_plan
    [UPD] Update maintenance_plan.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: maintenance-13.0/maintenance-13.0-maintenance_plan
    Translate-URL: https://translation.odoo-community.org/projects/maintenance-13-0/maintenance-13-0-maintenance_plan/

--- maintenance_plan/models/maintenance_kind.py
+++ maintenance_plan/models/maintenance_kind.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Camptocamp SA
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
