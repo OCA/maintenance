5eda2553ce89a270f61209337c007a025665b87d
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-14 09:51:53 +0100

    [IMP] maintenance_plan: black, isort

--- maintenance_plan/models/maintenance_request.py
+++ maintenance_plan/models/maintenance_request.py
@@ -7,13 +7,13 @@ from odoo import fields, models
 
 class MaintenanceRequest(models.Model):
 
-    _inherit = 'maintenance.request'
+    _inherit = "maintenance.request"
 
-    maintenance_kind_id = fields.Many2one(string='Maintenance kind',
-                                          comodel_name='maintenance.kind',
-                                          ondelete='restrict')
+    maintenance_kind_id = fields.Many2one(
+        string="Maintenance kind", comodel_name="maintenance.kind", ondelete="restrict"
+    )
 
-    maintenance_plan_id = fields.Many2one(string='Maintenance plan',
-                                          comodel_name='maintenance.plan',
-                                          ondelete='restrict')
-    note = fields.Html('Note')
+    maintenance_plan_id = fields.Many2one(
+        string="Maintenance plan", comodel_name="maintenance.plan", ondelete="restrict"
+    )
+    note = fields.Html("Note")
