ca48253864dfbcca3dd17a5c3cbef358521458eb
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-16 09:24:53 +0100

    [13.0][MIG] base_maintenance

--- base_maintenance/models/maintenance_team.py
+++ base_maintenance/models/maintenance_team.py
@@ -7,6 +7,5 @@ from odoo import fields, models
 class MaintenanceTeam(models.Model):
     _inherit = "maintenance.team"
 
-    active = fields.Boolean(default=True)
     user_id = fields.Many2one(comodel_name="res.users", string="Team Leader")
     description = fields.Text()
