c5d5fcc0e0ebc032485d0a72ce4928e8330fd146
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-24 13:46:59 +0100

    [12.0][MIG] cb_maintenance repo

--- maintenance_request_tags/tests/test_request_tags.py
+++ maintenance_request_tags/tests/test_request_tags.py
@@ -1,12 +1,14 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestRequestTags(TransactionCase):
-    def setUp(self):
-        super().setUp()
+class TestRequestTags(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        self = cls
         self.tag_1 = self.env["maintenance.request.tag"].create(
             {"name": "Test Tag 1"}
         )
@@ -23,9 +25,30 @@ class TestRequestTags(TransactionCase):
                 "parent_id": self.team_1.id,
             }
         )
+        self.equipment = self.env["maintenance.equipment"].create(
+            {"name": "Laptop"}
+        )
+
+        self.plan = self.env["maintenance.plan"].create(
+            {
+                "equipment_id": self.equipment.id,
+                "interval": 1,
+                "interval_step": "month",
+                "maintenance_plan_horizon": 2,
+                "planning_step": "month",
+                "maintenance_team_id": self.team_2.id,
+                "tag_ids": [(4, self.tag_2.id)],
+            }
+        )
 
     def test_request_tags(self):
         request = self.env["maintenance.request"].create(
             {"name": "Request", "maintenance_team_id": self.team_2.id}
         )
         self.assertEqual(len(request.selectable_tags_ids), 2)
+
+    def test_request_creation(self):
+        request = self.equipment._create_new_request(self.plan)
+        self.assertTrue(request)
+        for r in request:
+            self.assertTrue(r.tag_ids)
