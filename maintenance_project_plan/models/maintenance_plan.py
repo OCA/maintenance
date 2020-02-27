d809a3bb111d85b54439a8937628276a6c904e5e
David Alonso (Solvos) <david.alonso@solvos.es>
2020-02-27 16:56:44 +0100

    [IMP] maintenance_project_plan: black, isort

--- maintenance_project_plan/models/maintenance_plan.py
+++ maintenance_project_plan/models/maintenance_plan.py
@@ -1,28 +1,21 @@
 # Copyright 2019 Solvos Consultor??a Inform??tica (<http://www.solvos.es>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class MaintenancePlan(models.Model):
-    _inherit = 'maintenance.plan'
+    _inherit = "maintenance.plan"
 
-    project_id = fields.Many2one(
-        comodel_name='project.project',
-        ondelete='restrict')
-    task_id = fields.Many2one(
-        comodel_name='project.task')
+    project_id = fields.Many2one(comodel_name="project.project", ondelete="restrict")
+    task_id = fields.Many2one(comodel_name="project.task")
 
-    @api.onchange('project_id')
+    @api.onchange("project_id")
     def onchange_project_id(self):
         if self.project_id:
             if self.project_id != self.task_id.project_id:
                 self.task_id = False
-            return {'domain': {
-                'task_id': [('project_id', '=', self.project_id.id)]
-            }}
+            return {"domain": {"task_id": [("project_id", "=", self.project_id.id)]}}
         else:
             self.task_id = False
-            return {'domain': {
-                'task_id': [('project_id', '=', False)]
-            }}
+            return {"domain": {"task_id": [("project_id", "=", False)]}}
