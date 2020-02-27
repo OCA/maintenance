39d77e43493767bb96002428cf80074c74cfa714
David Alonso (Solvos) <david.alonso@solvos.es>
2020-02-27 16:56:44 +0100

    [IMP] maintenance_project_plan: black, isort

--- maintenance_project_plan/models/maintenance_equipment.py
+++ maintenance_project_plan/models/maintenance_equipment.py
@@ -5,16 +5,15 @@ from odoo import models
 
 
 class MaintenanceEquipment(models.Model):
-    _inherit = 'maintenance.equipment'
+    _inherit = "maintenance.equipment"
 
-    def _prepare_request_from_plan(
-        self, maintenance_plan, next_maintenance_date
-    ):
+    def _prepare_request_from_plan(self, maintenance_plan, next_maintenance_date):
         request = super()._prepare_request_from_plan(
-            maintenance_plan, next_maintenance_date)
+            maintenance_plan, next_maintenance_date
+        )
         if maintenance_plan.project_id:
-            request['project_id'] = maintenance_plan.project_id.id
+            request["project_id"] = maintenance_plan.project_id.id
         if maintenance_plan.task_id:
-            request['task_id'] = maintenance_plan.task_id.id
+            request["task_id"] = maintenance_plan.task_id.id
 
         return request
