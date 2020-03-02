d75c6a6d9be6b317828c4051fdb3d39d7d25f38b
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-02 10:30:49 +0100

    [MIG] maintenance_plan_activity: Migration to 13.0

--- maintenance_plan_activity/__manifest__.py
+++ maintenance_plan_activity/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 ForgeFlow S.L. (www.forgeflow.com)
+# Copyright 2019-20 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -7,10 +7,9 @@
         This module allows defining in the maintenance plan activities that
         will be created once the maintenance requests are created as a
         consequence of the plan itself.""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Eficent Business and IT Consulting Services S.L.,"
-    "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/maintenance",
     "depends": ["maintenance_plan"],
     "data": ["security/ir.model.access.csv", "views/maintenance_views.xml"],
