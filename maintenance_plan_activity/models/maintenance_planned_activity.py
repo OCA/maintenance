1c9a3d283e6a8f94cf723aadb2f0a82464cfdff6
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-02 09:54:07 +0100

    [IMP] maintenance_plan_activity: black, isort

--- maintenance_plan_activity/models/maintenance_planned_activity.py
+++ maintenance_plan_activity/models/maintenance_planned_activity.py
@@ -5,22 +5,18 @@ from odoo import fields, models
 
 
 class MiantenancePlannedactivity(models.Model):
-    _name = 'maintenance.planned.activity'
-    _description = 'Maintenance Planned Activity'
+    _name = "maintenance.planned.activity"
+    _description = "Maintenance Planned Activity"
 
     activity_type_id = fields.Many2one(
-        'mail.activity.type', 'Activity Type',
-        required=True,
+        "mail.activity.type", "Activity Type", required=True
     )
     user_id = fields.Many2one(
-        'res.users', 'Responsible',
-        default=lambda self: self.env.user,
+        "res.users", "Responsible", default=lambda self: self.env.user
     )
     date_before_request = fields.Integer(
-        '# Days before request',
+        "# Days before request",
         help="This is the number of days the due date of the activity will be"
-             "set before the Maintenance request scheduled date"
+        "set before the Maintenance request scheduled date",
     )
-    maintenance_plan_id = fields.Many2one(
-        'maintenance.plan', 'Maintenance Plan'
-    )
+    maintenance_plan_id = fields.Many2one("maintenance.plan", "Maintenance Plan")
