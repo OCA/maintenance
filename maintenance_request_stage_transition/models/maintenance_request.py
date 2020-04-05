7ddcc5d5bbcfa8eba0aacc32cf54b8774562a891
David Alonso <david.alonso@solvos.es>
2020-04-05 13:44:04 +0200

    [IMP] maintenance_request_stage_transition: black, isort, prettier

--- maintenance_request_stage_transition/models/maintenance_request.py
+++ maintenance_request_stage_transition/models/maintenance_request.py
@@ -1,34 +1,28 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from lxml import etree
+
 from odoo import api, fields, models
 from odoo.osv.orm import setup_modifiers
-from lxml import etree
 
 
 class MaintenanceRequest(models.Model):
 
-    _inherit = 'maintenance.request'
+    _inherit = "maintenance.request"
 
-    stage_id = fields.Many2one(
-        'maintenance.stage',
-        readonly=True
-    )
+    stage_id = fields.Many2one("maintenance.stage", readonly=True)
 
     @api.model
     def fields_view_get(
-            self, view_id=None, view_type="form", toolbar=False, submenu=False
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
     ):
         res = super().fields_view_get(
-            view_id=view_id,
-            view_type=view_type,
-            toolbar=toolbar,
-            submenu=submenu,
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu,
         )
         if view_type == "form":
             doc = etree.XML(res["arch"])
-            stages = self.env['maintenance.stage'].search(
-                [], order="sequence desc")
+            stages = self.env["maintenance.stage"].search([], order="sequence desc")
             header = doc.xpath("//form/header")[0]
             for stage in stages:
                 node = stage._get_stage_node()
@@ -38,12 +32,9 @@ class MaintenanceRequest(models.Model):
         return res
 
     def set_maintenance_stage(self):
-        if not self.env.context.get('next_stage_id'):
+        if not self.env.context.get("next_stage_id"):
             return {}
-        return self._set_maintenance_stage(
-            self.env.context.get('next_stage_id'))
+        return self._set_maintenance_stage(self.env.context.get("next_stage_id"))
 
     def _set_maintenance_stage(self, stage_id):
-        self.write({
-            'stage_id': stage_id
-        })
+        self.write({"stage_id": stage_id})
