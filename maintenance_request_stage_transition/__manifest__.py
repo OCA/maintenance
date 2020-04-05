e6838f9bed3433020eecc2640e67d46dff23d4a0
David Alonso <david.alonso@solvos.es>
2020-04-05 20:58:44 +0200

    [MIG] maintenance_request_stage_transition: Migration to 13.0

--- maintenance_request_stage_transition/__manifest__.py
+++ maintenance_request_stage_transition/__manifest__.py
@@ -5,11 +5,12 @@
     "name": "Maintenance Request Stage transition",
     "summary": """
         Manage transition visibility and management between stages""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
     "depends": ["maintenance"],
-    "data": ["views/maintenance_request.xml", "views/maintenance_stage.xml",],
-    "maintainers": ["etobella",],
+    "data": ["views/maintenance_request.xml", "views/maintenance_stage.xml"],
+    "demo": ["data/demo_maintenance_request_stage_transition.xml"],
+    "maintainers": ["etobella"],
 }
