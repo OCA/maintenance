9c96cd480df7674cb3b722299b4bac73e5f0c16f
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-04-14 15:52:09 +0200

    [12.0][MIG] maintenance_remote

--- maintenance_remote/__manifest__.py
+++ maintenance_remote/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Maintenance Remote',
     'summary': """
         Define remote on maintenance request""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/maintenance',
