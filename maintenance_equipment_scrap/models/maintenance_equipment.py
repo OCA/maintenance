0363a25012ac0869196267631151856dec5e7f4c
Andrea <a.stirpe@onestein.nl>
2020-05-07 14:52:18 +0200

    pre-commit

--- maintenance_equipment_scrap/models/maintenance_equipment.py
+++ maintenance_equipment_scrap/models/maintenance_equipment.py
@@ -6,28 +6,28 @@ from odoo import api, fields, models
 
 class MaintenanceEquipment(models.Model):
 
-    _inherit = 'maintenance.equipment'
+    _inherit = "maintenance.equipment"
 
     equipment_scrap_template_id = fields.Many2one(
-        'mail.template',
-        string='Equipment Scrap Email Template',
+        "mail.template", string="Equipment Scrap Email Template",
     )
 
     @api.multi
     def action_perform_scrap(self):
         self.ensure_one()
         action = self.env.ref(
-            'maintenance_equipment_scrap.wizard_perform_equipment_scrap_action'
+            "maintenance_equipment_scrap.wizard_perform_equipment_scrap_action"
         )
         result = action.read()[0]
         return result
 
     @api.multi
-    @api.onchange('category_id')
+    @api.onchange("category_id")
     def onchange_category_id(self):
         for equipment in self:
             if equipment.category_id:
-                equipment.equipment_scrap_template_id = \
+                equipment.equipment_scrap_template_id = (
                     equipment.category_id.equipment_scrap_template_id
+                )
             else:
                 equipment.equipment_scrap_template_id = None
