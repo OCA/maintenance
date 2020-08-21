eb63cd6a32cc4c413316988aa00e7aa71bff65c2
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-08-21 13:29:48 -0300

    [IMP] base_maintenance: Add a Maintenance Request Report

--- base_maintenance/__manifest__.py
+++ base_maintenance/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Base Maintenance",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "development_status": "Beta",
     "website": "https://www.github.com/OCA/maintenance",
--- base_maintenance/readme/CONTRIBUTORS.rst
+++ base_maintenance/readme/CONTRIBUTORS.rst
@@ -1,2 +1,2 @@
-* Lois Rilo <lois.rilo@eficent.com>
+* Lois Rilo <lois.rilo@forgeflow.com>
 * Marcel Savegnago <marcel.savegnago@escodoo.com.br>
