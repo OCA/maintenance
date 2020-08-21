c7318bcadc30336a48ecf96d0408ae92c6ad82ee
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-08-21 16:31:28 -0300

    [ADD] link button to maintenances

--- maintenance_request_repair/models/repair_order.py
+++ maintenance_request_repair/models/repair_order.py
@@ -1,7 +1,7 @@
 # Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class RepairOrder(models.Model):
@@ -13,3 +13,27 @@ class RepairOrder(models.Model):
         'repair_order_id',
         string="Maintenance Requests"
     )
+
+    maintenance_request_count = fields.Integer(
+        compute='_compute_maintenance_request_count',
+        string='# Maintenances'
+    )
+
+    @api.depends('maintenance_request_ids')
+    def _compute_maintenance_request_count(self):
+        for repair in self:
+            repair.maintenance_request_count = len(repair.maintenance_request_ids)
+
+    def action_view_maintenance(self):
+        return {
+            'name': 'Child equipment of %s' % self.name,
+            'type': 'ir.actions.act_window',
+            'res_model': 'maintenance.request',
+            'res_id': self.id,
+            'view_mode': 'list,form',
+            'context': {
+                **self.env.context,
+                'default_repair_order_id': self.id,
+                'repair_order_id_editable': False},
+            'domain': [('id', 'in', self.maintenance_request_ids.ids)],
+        }
