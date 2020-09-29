623564e403e9e9780a26e2e85f2fc207eb0cfbd5
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-29 11:13:03 +0200

    [13.0][MIG] maintenance_team_hierarchy

--- maintenance_team_hierarchy/models/maintenance_team.py
+++ maintenance_team_hierarchy/models/maintenance_team.py
@@ -15,7 +15,18 @@ class MaintenanceTeam(models.Model):
     parent_path = fields.Char(index=True)
 
     request_ids = fields.Many2many(
-        "maintenance.request", compute="_compute_request_ids"
+        "maintenance.request", compute="_compute_request_ids",
+    )
+
+    todo_request_ids = fields.One2many(compute="_compute_new_todo_requests")
+    todo_request_count = fields.Integer(compute="_compute_new_todo_requests")
+    todo_request_count_date = fields.Integer(compute="_compute_new_todo_requests")
+    todo_request_count_high_priority = fields.Integer(
+        compute="_compute_new_todo_requests"
+    )
+    todo_request_count_block = fields.Integer(compute="_compute_new_todo_requests")
+    todo_request_count_unscheduled = fields.Integer(
+        compute="_compute_new_todo_requests"
     )
 
     def _compute_request_ids(self):
@@ -27,23 +38,29 @@ class MaintenanceTeam(models.Model):
     def _get_request_domains(self):
         return [("maintenance_team_id", "child_of", self.id)]
 
-    @api.one
     @api.depends()
+    def _compute_new_todo_requests(self):
+        """ This is the only way to remove the api depends fields. This is necessary
+        because request_ids is now a computed field
+        """
+        self._compute_todo_requests()
+
     def _compute_todo_requests(self):
         """This function improves the computation and simplifies the data"""
         request_obj = self.env["maintenance.request"]
-        domain = self._get_request_domains() + [("stage_id.done", "=", False)]
-        self.todo_request_ids = request_obj.search(domain)
-        self.todo_request_count = request_obj.search_count(domain)
-        self.todo_request_count_date = request_obj.search_count(
-            domain + [("schedule_date", "!=", False)]
-        )
-        self.todo_request_count_high_priority = request_obj.search_count(
-            domain + [("priority", "=", "3")]
-        )
-        self.todo_request_count_block = request_obj.search_count(
-            domain + [("kanban_state", "=", "blocked")]
-        )
-        self.todo_request_count_unscheduled = request_obj.search_count(
-            domain + [("schedule_date", "=", False)]
-        )
+        for record in self:
+            domain = record._get_request_domains() + [("stage_id.done", "=", False)]
+            record.todo_request_ids = request_obj.search(domain)
+            record.todo_request_count = request_obj.search_count(domain)
+            record.todo_request_count_date = request_obj.search_count(
+                domain + [("schedule_date", "!=", False)]
+            )
+            record.todo_request_count_high_priority = request_obj.search_count(
+                domain + [("priority", "=", "3")]
+            )
+            record.todo_request_count_block = request_obj.search_count(
+                domain + [("kanban_state", "=", "blocked")]
+            )
+            record.todo_request_count_unscheduled = request_obj.search_count(
+                domain + [("schedule_date", "=", False)]
+            )
