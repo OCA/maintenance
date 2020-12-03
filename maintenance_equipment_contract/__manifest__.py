fb9cc30aa17a6ac62c73b712c8a9bcb2bc70db1e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-12-03 12:29:55 +0100

    [IMP] maintenance_equipment_contract: black, isort, prettier

--- maintenance_equipment_contract/__manifest__.py
+++ maintenance_equipment_contract/__manifest__.py
@@ -2,22 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Maintenance Equipment Contract',
-    'summary': """
+    "name": "Maintenance Equipment Contract",
+    "summary": """
         Manage equipment contracts""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/maintenance',
-    'depends': [
-        'contract',
-        'base_maintenance',
-    ],
-    'data': [
-        'views/contract_contract.xml',
-        'views/maintenance_equipment.xml',
-    ],
-    'demo': [
-        'demo/maintenance_equipment_contract_demo.xml',
-    ]
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/maintenance",
+    "depends": ["contract", "base_maintenance",],
+    "data": ["views/contract_contract.xml", "views/maintenance_equipment.xml",],
+    "demo": ["demo/maintenance_equipment_contract_demo.xml",],
 }
--- maintenance_equipment_contract/models/maintenance_equipment.py
+++ maintenance_equipment_contract/models/maintenance_equipment.py
@@ -6,37 +6,32 @@ from odoo import api, fields, models
 
 class MaintenanceEquipment(models.Model):
 
-    _inherit = 'maintenance.equipment'
+    _inherit = "maintenance.equipment"
 
-    contract_ids = fields.Many2many(
-        'contract.contract', string='Contracts'
-    )
-    contract_count = fields.Integer(
-        compute='_compute_contract_count',
-    )
+    contract_ids = fields.Many2many("contract.contract", string="Contracts")
+    contract_count = fields.Integer(compute="_compute_contract_count",)
 
-    @api.depends('contract_ids')
+    @api.depends("contract_ids")
     def _compute_contract_count(self):
         for record in self:
             record.contract_count = len(record.contract_ids.ids)
 
     @api.multi
     def action_view_contracts(self):
-        action = self.env.ref(
-            'contract.action_customer_contract').read()[0]
+        action = self.env.ref("contract.action_customer_contract").read()[0]
         if len(self.contract_ids) > 1:
-            action['domain'] = [('id', 'in', self.contract_ids.ids)]
+            action["domain"] = [("id", "in", self.contract_ids.ids)]
         elif self.contract_ids:
-            action['views'] = [(self.env.ref(
-                'contract.contract_contract_form_view'
-            ).id, 'form')]
-            action['res_id'] = self.contract_ids.id
-        action['context'] = {
-            'default_equipment_ids': self.ids,
-            'is_contract': 1,
-            'search_default_not_finished': 1,
-            'search_default_recurring_invoices': 1,
-            'default_recurring_invoices': 1,
-            'default_contract_type': 'purchase'
+            action["views"] = [
+                (self.env.ref("contract.contract_contract_form_view").id, "form")
+            ]
+            action["res_id"] = self.contract_ids.id
+        action["context"] = {
+            "default_equipment_ids": self.ids,
+            "is_contract": 1,
+            "search_default_not_finished": 1,
+            "search_default_recurring_invoices": 1,
+            "default_recurring_invoices": 1,
+            "default_contract_type": "purchase",
         }
         return action
