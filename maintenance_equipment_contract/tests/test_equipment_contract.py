7699bb33d1a0503f7fc55ed5e4d070d86eadfcc0
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-12-03 12:29:55 +0100

    [IMP] maintenance_equipment_contract: black, isort, prettier

--- maintenance_equipment_contract/tests/test_equipment_contract.py
+++ maintenance_equipment_contract/tests/test_equipment_contract.py
@@ -5,29 +5,32 @@ from odoo.tests.common import TransactionCase
 
 
 class TestEquipmentContract(TransactionCase):
-
     def setUp(self):
         super().setUp()
-        self.partner = self.env['res.partner'].create({'name': 'Test partner'})
-        self.equipment_id = self.env['maintenance.equipment'].create({
-            'name': 'Equipment'
-        })
-        self.contract = self.env['contract.contract'].create({
-            'name': 'Contract',
-            'partner_id': self.partner.id,
-            'equipment_ids': [(4, self.equipment_id.id)]
-        })
+        self.partner = self.env["res.partner"].create({"name": "Test partner"})
+        self.equipment_id = self.env["maintenance.equipment"].create(
+            {"name": "Equipment"}
+        )
+        self.contract = self.env["contract.contract"].create(
+            {
+                "name": "Contract",
+                "partner_id": self.partner.id,
+                "equipment_ids": [(4, self.equipment_id.id)],
+            }
+        )
 
     def test_equipment_contract(self):
         self.assertEqual(self.equipment_id.contract_count, 1)
         action = self.equipment_id.action_view_contracts()
-        self.assertEqual(action['res_id'], self.contract.id)
+        self.assertEqual(action["res_id"], self.contract.id)
 
-        self.env['contract.contract'].create({
-            'name': 'Contract 2',
-            'partner_id': self.partner.id,
-            'equipment_ids': [(4, self.equipment_id.id)]
-        })
+        self.env["contract.contract"].create(
+            {
+                "name": "Contract 2",
+                "partner_id": self.partner.id,
+                "equipment_ids": [(4, self.equipment_id.id)],
+            }
+        )
         self.assertEqual(self.equipment_id.contract_count, 2)
         action = self.equipment_id.action_view_contracts()
-        self.assertIn('domain', action.keys())
+        self.assertIn("domain", action.keys())
