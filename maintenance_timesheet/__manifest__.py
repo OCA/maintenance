047cdfe31d3f94c431a2f42c35257729490978a6
David Alonso <david.alonso@solvos.es>
2020-12-17 13:17:52 +0100

    [IMP] maintenance_timesheet: add report support for requests

--- maintenance_timesheet/__manifest__.py
+++ maintenance_timesheet/__manifest__.py
@@ -1,4 +1,4 @@
-# © 2019 Solvos Consultoría Informática (<http://www.solvos.es>)
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Maintenance Timesheets",
@@ -13,6 +13,7 @@
         "security/maintenance_timesheet_security.xml",
         "views/hr_timesheet_views.xml",
         "views/maintenance_request_views.xml",
+        "report/maintenance_request_report.xml",
     ],
     "demo": ["data/demo_maintenance_timesheet.xml"],
     "installable": True,
