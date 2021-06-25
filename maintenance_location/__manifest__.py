07b61d4ba910d6f51125cad7d3ca55cd31665c3c
Olga Marco <olga.marco@creublanca.es>
2021-06-25 08:44:52 +0200

    [MIG] maintenance_location: Migration to 13.0

--- maintenance_location/__manifest__.py
+++ maintenance_location/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Maintenance Location",
     "summary": """
         Define a location system for maintenance""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
