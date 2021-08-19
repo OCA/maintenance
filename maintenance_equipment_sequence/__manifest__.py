73a80f767419c3ba65164955c8b1786654c578a6
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-08-19 09:49:32 +0200

    [MIG] maintenance_equipment_sequence: Migration to 14.0

--- maintenance_equipment_sequence/__manifest__.py
+++ maintenance_equipment_sequence/__manifest__.py
@@ -6,9 +6,10 @@
     "summary": """
         Adds sequence to maintenance equipment defined in the equipment's
         category""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow S.L.," "Odoo Community Association (OCA)",
+    "maintainers": ["AdriaGForgeFlow"],
     "website": "https://github.com/OCA/maintenance",
     "depends": ["maintenance"],
     "data": ["views/maintenance_views.xml"],
