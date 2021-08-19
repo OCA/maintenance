3b857cb2526c3df205f2c7b07ecb444433be8972
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-08-19 10:19:36 +0200

    [MIG] maintenance_equipment_tags: Migration to 14.0
    [UPD] Update maintenance_equipment_tags.pot
    
    [UPD] README.rst

--- maintenance_equipment_tags/__manifest__.py
+++ maintenance_equipment_tags/__manifest__.py
@@ -5,9 +5,10 @@
     "name": "Maintenance Equipment Tags",
     "summary": """
         Adds category tags to equipment""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "maintainers": ["etobella"],
     "website": "https://github.com/OCA/maintenance",
     "depends": ["maintenance"],
     "demo": ["demo/maintenance_equipment_tags_demo.xml"],
