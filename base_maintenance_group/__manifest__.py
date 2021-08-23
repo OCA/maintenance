94f6280669bf12b751f37fdbe07233dbb4340193
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-08-23 10:51:43 +0200

    [MIG]  base_maintenance_group: Migration to 13.0

--- base_maintenance_group/__manifest__.py
+++ base_maintenance_group/__manifest__.py
@@ -7,9 +7,9 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
     "category": "Maintenance",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["maintenance",],
+    "depends": ["maintenance"],
     "data": [
         "data/maintenance_data.xml",
         "security/maintenance_security.xml",
