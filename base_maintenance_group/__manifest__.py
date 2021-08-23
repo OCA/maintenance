f97dce9dc95b84181da883508ebf1ab443e31619
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-08-23 10:50:17 +0200

    [IMP]  base_maintenance_group: black, isort, prettier

--- base_maintenance_group/__manifest__.py
+++ base_maintenance_group/__manifest__.py
@@ -2,19 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Maintenance Groups',
-    'summary': 'Provides base access groups for the Maintenance App',
-    'author': 'Onestein, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/maintenance',
-    'category': 'Maintenance',
-    'version': '12.0.1.1.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'maintenance',
-    ],
-    'data': [
-        'data/maintenance_data.xml',
-        'security/maintenance_security.xml',
-        'menuitems.xml',
+    "name": "Maintenance Groups",
+    "summary": "Provides base access groups for the Maintenance App",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/maintenance",
+    "category": "Maintenance",
+    "version": "12.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": ["maintenance",],
+    "data": [
+        "data/maintenance_data.xml",
+        "security/maintenance_security.xml",
+        "menuitems.xml",
     ],
 }
