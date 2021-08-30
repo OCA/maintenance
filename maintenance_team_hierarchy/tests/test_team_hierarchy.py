3717ebf2e35d47dfcf7c04a948161ec6a4c71c21
davidborromeo <david.borromeo@forgeflow.com>
2021-08-30 13:41:30 +0200

    [MIG] maintenance_team_hierarchy: Migration to 14.0
    [UPD] Update maintenance_team_hierarchy.pot
    
    [UPD] README.rst

--- maintenance_team_hierarchy/tests/test_team_hierarchy.py
+++ maintenance_team_hierarchy/tests/test_team_hierarchy.py
@@ -1,11 +1,11 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase, at_install, post_install
+from odoo.tests.common import TransactionCase, tagged
 
 
-@at_install(False)
-@post_install(True)
+# This test should only be executed after all modules have been installed.
+@tagged("post_install", "-at_install")
 class TestTeamHierarchy(TransactionCase):
     def setUp(self):
         super().setUp()
