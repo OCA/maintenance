fbbc918893ab8cb49b2cc6b9199a716ca0345cdc
ChristianSantamaria <christiancameselle@hotmail.com>
2021-09-02 13:09:37 +0200

    [IMP] maintenance_equipment_category_kanban: black, isort, prettier

--- maintenance_equipment_category_kanban/__manifest__.py
+++ maintenance_equipment_category_kanban/__manifest__.py
@@ -2,20 +2,17 @@
 # License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
 
 {
-    'name': 'Maintenance Equipment Category Kanban',
-    'summary': """
+    "name": "Maintenance Equipment Category Kanban",
+    "summary": """
         Sets kanban category groping by default for equipments
         """,
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Solvos,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.github.com/OCA/maintenance',
-    'depends': [
-        'maintenance'
-    ],
-    'data': [
-        'views/maintenance_equipment_category_views.xml',
-        'views/maintenance_equipment_views.xml',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Solvos," "Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/maintenance",
+    "depends": ["maintenance"],
+    "data": [
+        "views/maintenance_equipment_category_views.xml",
+        "views/maintenance_equipment_views.xml",
     ],
 }
--- maintenance_equipment_category_kanban/models/maintenance_equipment_category.py
+++ maintenance_equipment_category_kanban/models/maintenance_equipment_category.py
@@ -1,7 +1,7 @@
 # © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MaintenanceEquipmentCategory(models.Model):
