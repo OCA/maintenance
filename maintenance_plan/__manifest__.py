5426cd4df51857c749a105f37f3cee583fee7091
Enric Tobella <etobella@creublanca.es>
2021-09-29 17:25:58 +0200

    [FIX] maintenance_plan: Remake the computation of next date * Use latest request_date both for done and todo searches. * When there is a latest done apply interval
    Co-authored-by: Lois Rilo <lois.rilo@forgeflow.com>
    Co-authored-by: LoisRForgeFlow <LoisRForgeFlow@users.noreply.github.com>
    
    maintenance_plan 14.0.1.0.1

--- maintenance_plan/__manifest__.py
+++ maintenance_plan/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Maintenance Plan",
     "summary": "Extends preventive maintenance planning",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Camptocamp SA, ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Maintenance",
