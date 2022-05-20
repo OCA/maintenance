3902fc5654a05d5138313420de99c928c928774b
Olga Marco <olga.marco@creublanca.es>
2022-05-20 10:16:27 +0200

    [MIG] maintenance_request_sequence: Migration to 15.0

--- maintenance_request_sequence/__manifest__.py
+++ maintenance_request_sequence/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Maintenance Request Sequence",
     "summary": """
         Adds sequence to maintenance requests""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
