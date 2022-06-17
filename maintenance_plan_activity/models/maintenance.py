39803573c592754d93f06f34b8cc9aff6d9bb5d7
Miquel Raïch <miquel.raich@forgeflow.com>
2022-06-17 10:51:54 +0200

    [FIX] maintenance_plan: avoid error if planning_step is not defined
    Also, clean-up of useless from_string() calls.

--- maintenance_plan_activity/models/maintenance.py
+++ maintenance_plan_activity/models/maintenance.py
@@ -21,7 +21,7 @@ class MaintenanceEquipment(models.Model):
         new_requests = super()._create_new_request(maintenance_plan)
         for request in new_requests:
             for planned_activity in maintenance_plan.planned_activity_ids:
-                # In case mail_activty_team is installed this makes sure
+                # In case mail_activity_team is installed this makes sure
                 # the correct activity team is selected. If that module is
                 # not installed the context does nothing
                 self.env["mail.activity"].with_context(
@@ -30,14 +30,14 @@ class MaintenanceEquipment(models.Model):
                     {
                         "activity_type_id": planned_activity.activity_type_id.id,
                         "note": _(
-                            "Activity automatically generated from " "maintenance plan"
+                            "Activity automatically generated from maintenance plan"
                         ),
                         "user_id": planned_activity.user_id.id or self.env.user.id,
                         "res_id": request.id,
                         "res_model_id": self.env.ref(
                             "maintenance.model_maintenance_request"
                         ).id,
-                        "date_deadline": fields.Date.from_string(request.schedule_date)
+                        "date_deadline": request.schedule_date
                         - timedelta(days=planned_activity.date_before_request),
                     }
                 )
