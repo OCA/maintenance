0147584c0e4cab55367a84852a88f220632bc267
Kevin Luna <kevin.luna@creublanca.es>
2022-11-08 09:55:44 +0100

    [MIG] maintenance_location: Migration to 14.0

--- maintenance_location/__manifest__.py
+++ maintenance_location/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Maintenance Location",
     "summary": """
         Define a location system for maintenance""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-maintenance",
     "depends": ["maintenance_plan", "web_widget_child_selector"],
     "data": [
