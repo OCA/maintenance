115bd9c5732d73832e8564ab0c41d52a4d5fefae
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-08 13:13:53 +0100

    [14.0][MIG] maintenance_request_tags: Migration to 14.0

--- maintenance_request_tags/__manifest__.py
+++ maintenance_request_tags/__manifest__.py
@@ -5,15 +5,15 @@
     "name": "Maintenance Request Tags",
     "summary": """
         Adds tags to Maintenance Requests""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-maintenance",
     "depends": ["maintenance_plan", "maintenance_team_hierarchy"],
     "data": [
+        "security/ir.model.access.csv",
         "views/maintenance_equipment.xml",
         "views/maintenance_plan.xml",
-        "security/ir.model.access.csv",
         "views/maintenance_request_tag.xml",
         "views/maintenance_request.xml",
         "views/maintenance_team.xml",
