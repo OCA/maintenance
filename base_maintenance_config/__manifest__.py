0f7724579af7960ea13ec4694e28f5470b715c8e
Stefano Consolaro <stefano.consolaro@mymage.it>
2022-11-23 23:47:46 +0100

    [MIG] base_maintenance_config: Migration to 15.0
    [UPD] Update base_maintenance_config.pot
    
    [UPD] README.rst
    
    [UPD] README.rst

--- base_maintenance_config/__manifest__.py
+++ base_maintenance_config/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
     "category": "Maintenance",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["maintenance"],
     "data": ["views/res_config_settings.xml"],
