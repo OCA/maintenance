38e9e5424008ca7c4048be09808aa43b4e10cb62
Víctor Martínez <victor.martinez@tecnativa.com>
2023-01-05 13:33:50 +0100

    [MIG] maintenance_timesheet_time_control: Migration to 15.0
    TT40942
    
    maintenance_timesheet_time_control
    
    maintenance_timesheet_time_control

--- maintenance_timesheet_time_control/__manifest__.py
+++ maintenance_timesheet_time_control/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Maintenance Timesheets Time Control",
     "summary": "Maintenance Timesheets Timesheet Time Control",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "category": "Hidden",
     "author": "Tecnativa,Odoo Community Association (OCA)",
