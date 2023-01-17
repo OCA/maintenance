51c089f5ca41d56082f185b079f16491f92981e8
Maxime Franco <maxime.franco@acsone.eu>
2023-01-17 10:02:47 +0100

    [MIG] maintenance_equipment_image: Migration to 16.0

--- maintenance_equipment_image/__manifest__.py
+++ maintenance_equipment_image/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Maintenance Equipment Image",
     "summary": """Adds images to equipment.""",
     "category": "Manufacturing/Maintenance",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Exo Software," "Odoo Community Association (OCA)",
     "maintainers": ["pedrocasi"],
