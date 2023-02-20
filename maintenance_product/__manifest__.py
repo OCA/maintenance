17b9c077f6ab9bd6d322c4167bf016903aa91f83
Víctor Martínez <victor.martinez@tecnativa.com>
2023-02-20 09:23:01 +0100

    [MIG] maintenance_product: Migration to 13.0
    TT41743

--- maintenance_product/__manifest__.py
+++ maintenance_product/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Maintenance Product",
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Maintenance",
     "website": "https://github.com/OCA/maintenance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
