034a5408f1274adda1f6bf02c899836277818ebb
Kaynnan Bardauil <kaynnanx1@gmail.com>
2023-03-15 12:27:34 -0300

    [MIG] maintenance_request_repair: Migration to 14.0

--- maintenance_request_repair/__manifest__.py
+++ maintenance_request_repair/__manifest__.py
@@ -2,20 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Maintenance Request Repair',
-    'summary': """
+    "name": "Maintenance Request Repair",
+    "summary": """
         This is a bridge module between Maintenance and Repair""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'category': 'Maintenance',
-    'author': 'Escodoo,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/maintenance',
-    'depends': [
-        'maintenance',
-        'repair',
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Maintenance",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/maintenance",
+    "depends": [
+        "maintenance",
+        "repair",
     ],
-    'data': [
-        'views/maintenance_request.xml',
-        'views/repair_order.xml',
+    "data": [
+        "views/maintenance_request.xml",
+        "views/repair_order.xml",
     ],
 }
