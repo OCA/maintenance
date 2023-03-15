f512634d03d945e02263ced49439a5a5f5c938c5
Kaynnan Bardauil <kaynnanx1@gmail.com>
2023-03-15 12:27:23 -0300

    [IMP] maintenance_request_repair: black, isort, prettier

--- maintenance_request_repair/models/maintenance_request.py
+++ maintenance_request_repair/models/maintenance_request.py
@@ -6,6 +6,6 @@ from odoo import fields, models
 
 class MaintenanceRequest(models.Model):
 
-    _inherit = 'maintenance.request'
+    _inherit = "maintenance.request"
 
-    repair_order_id = fields.Many2one('repair.order', 'Repair Order')
+    repair_order_id = fields.Many2one("repair.order", "Repair Order")
