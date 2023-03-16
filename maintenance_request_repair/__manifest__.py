29bed5b8af0988a06b909ff98a8afbca58338c46
Kaynnan Bardauil <kaynnanx1@gmail.com>
2023-03-15 21:01:18 -0300

    [MIG] maintenance_request_repair: Migration to 15.0

--- maintenance_request_repair/__manifest__.py
+++ maintenance_request_repair/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Maintenance Request Repair",
     "summary": """
         This is a bridge module between Maintenance and Repair""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "category": "Maintenance",
     "author": "Escodoo,Odoo Community Association (OCA)",
