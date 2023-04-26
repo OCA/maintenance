91aa776762918af4c3923459126cac0a06bb3a03
Martha Rondon <mrondon@archeti.com>
2023-04-25 20:12:00 -0400

    [MIG] maintenance_request_employee to 16.0

--- maintenance_request_employee/__manifest__.py
+++ maintenance_request_employee/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Maintenance Request Employee",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Maintenance",
     "website": "https://github.com/OCA/maintenance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
