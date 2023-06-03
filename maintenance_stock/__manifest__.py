63d14c7dfb6c64391f732de96df5459ada7cfc59
Manuel Marquez <mmarquez@indexacorp.com>
2023-06-03 18:17:07 -0400

    [IMP] maintenance_stock: pre-commit execution

--- maintenance_stock/__manifest__.py
+++ maintenance_stock/__manifest__.py
@@ -8,10 +8,7 @@
     "version": "12.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/maintenance",
-    "depends": [
-        "base_maintenance",
-        "stock",
-    ],
+    "depends": ["base_maintenance", "stock",],
     "data": [
         "views/maintenance_equipment_views.xml",
         "views/maintenance_request_views.xml",
@@ -19,9 +16,7 @@
         "views/stock_move_line_views.xml",
         "views/stock_picking_views.xml",
     ],
-    "demo": [
-        "data/demo_maintenance_stock.xml",
-    ],
+    "demo": ["data/demo_maintenance_stock.xml",],
     "post_init_hook": "post_init_hook",
-    'installable': True,
+    "installable": True,
 }
