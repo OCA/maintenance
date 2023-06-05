49c90d1ed51eb191da32181bd9ade20c4d6c1ace
David Alonso // Solvos <david.alonso@solvos.es>
2023-06-05 12:34:13 +0200

    [MIG] maintenance_project: Migration to 16.0

--- maintenance_project/models/maintenance_request.py
+++ maintenance_project/models/maintenance_request.py
@@ -10,19 +10,20 @@ class MaintenanceRequest(models.Model):
     project_id = fields.Many2one(comodel_name="project.project")
     task_id = fields.Many2one(comodel_name="project.task")
 
-    @api.model
-    def create(self, values):
+    @api.model_create_multi
+    def create(self, vals_list):
         """
         We ensure for appropiate project and task for new requests,
         specially the automatically generated ones
         """
-        newreq = super().create(values)
-        if not newreq.project_id and newreq.equipment_id:
-            newreq.project_id = newreq.equipment_id.project_id
-        if not newreq.task_id and newreq.maintenance_type == "preventive":
-            newreq.task_id = newreq.equipment_id.preventive_default_task_id
+        newreqs = super().create(vals_list)
+        for newreq in newreqs:
+            if not newreq.project_id and newreq.equipment_id:
+                newreq.project_id = newreq.equipment_id.project_id
+            if not newreq.task_id and newreq.maintenance_type == "preventive":
+                newreq.task_id = newreq.equipment_id.preventive_default_task_id
 
-        return newreq
+        return newreqs
 
     @api.onchange("equipment_id")
     def onchange_equipment_id(self):
