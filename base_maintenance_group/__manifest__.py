065dc361ab699dad30fd22ec63f209ec47d444c1
Borruso <gborruso@dinamicheaziendali.it>
2023-06-30 12:00:58 +0200

    [MIG] base_maintenance_group: Migration to 16.0

--- base_maintenance_group/__manifest__.py
+++ base_maintenance_group/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
     "category": "Maintenance",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["maintenance"],
     "data": [
