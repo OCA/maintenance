97fed85b88c791a73303f0d3ad5392fc7a720aab
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-03 12:49:34 -0300

    [MIG] maintenance_equipment_tags: Migration to 16.0

--- maintenance_equipment_tags/__manifest__.py
+++ maintenance_equipment_tags/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Maintenance Equipment Tags",
     "summary": """
         Adds category tags to equipment""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "maintainers": ["etobella"],
