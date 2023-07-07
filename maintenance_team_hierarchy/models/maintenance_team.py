2639d27e53071564d59425cf6874fa238ce6dc3d
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-07 12:43:50 -0300

    [MIG] maintenance_team_hierarchy: Migration to 16.0

--- maintenance_team_hierarchy/models/maintenance_team.py
+++ maintenance_team_hierarchy/models/maintenance_team.py
@@ -12,7 +12,7 @@ class MaintenanceTeam(models.Model):
     _inherit = "maintenance.team"
 
     parent_id = fields.Many2one("maintenance.team", ondelete="restrict")
-    parent_path = fields.Char(index=True)
+    parent_path = fields.Char(index=True, unaccent=False)
 
     request_ids = fields.Many2many(
         "maintenance.request",
