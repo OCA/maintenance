f6f9545a6e2f4dddb736a3f3fb4d15ff06a948c6
Christian-RB <c.ramos@binhex.es>
2023-07-20 12:47:58 +0000

    [16.0][FIX] base_maintenance: style on ButtonBox and colspan

--- base_maintenance/__manifest__.py
+++ base_maintenance/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Base Maintenance",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "development_status": "Beta",
     "website": "https://github.com/OCA/maintenance",
