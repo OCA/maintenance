11571f5ed6762bfc9fc3b574ad0717d52fc80919
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-11-09 08:22:09 -0300

    [MIG] maintenance_security: Migration to 16.0

--- maintenance_security/__manifest__.py
+++ maintenance_security/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2023 Tecnativa - Víctor Martínez
 # Copyright 2023 Tecnativa - Pedro M. Baeza
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Maintenance Security",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/maintenance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
