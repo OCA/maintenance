59b2411002a7957764e7631c335fad8d8d402f2a
David Alonso // Solvos <david.alonso@solvos.es>
2024-02-20 12:11:32 +0100

    [UPD] maintenance_equipment_custom_info: restore authorship

--- maintenance_equipment_custom_info/__manifest__.py
+++ maintenance_equipment_custom_info/__manifest__.py
@@ -7,7 +7,7 @@
         Add Custom Info on Maintenance Equipments""",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Dixmit,Odoo Community Association (OCA)",
+    "author": "Solvos,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
     "depends": ["base_custom_info", "maintenance"],
     "data": ["views/maintenance_equipment.xml"],
