5edba7360a6f8044beda5f694399dd6fa820cbf1
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2024-03-14 18:13:30 +0100

    [16][MIG] maintenance_request_purchase: migrate to version 16.0

--- maintenance_request_purchase/tests/test_maintenance_purchase.py
+++ maintenance_request_purchase/tests/test_maintenance_purchase.py
@@ -1,10 +1,10 @@
 # Copyright 2023 Dixmit
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestMaintenancePurchase(SavepointCase):
+class TestMaintenancePurchase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
