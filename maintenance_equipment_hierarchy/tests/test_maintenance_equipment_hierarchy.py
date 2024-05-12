da8835fa7d52a7ee112bc84572d275c48b37a54a
David Alonso // Solvos <david.alonso@solvos.es>
2024-05-12 11:14:52 +0200

    [MIG] maintenance_equipment_hierarchy: Migration to 17.0

--- maintenance_equipment_hierarchy/tests/test_maintenance_equipment_hierarchy.py
+++ maintenance_equipment_hierarchy/tests/test_maintenance_equipment_hierarchy.py
@@ -6,12 +6,13 @@ from odoo.tests import common
 
 
 class TestMaintenanceEquipmentHierarchy(common.TransactionCase):
-    def setUp(self):
-        super(TestMaintenanceEquipmentHierarchy, self).setUp()
-        self.Equipment = self.env["maintenance.equipment"]
-        self.equipment1 = self.Equipment.create({"name": "Equipment 1"})
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.Equipment = cls.env["maintenance.equipment"]
+        cls.equipment1 = cls.Equipment.create({"name": "Equipment 1"})
 
-        self.equipment1_1 = self.Equipment.create({"name": "Equipment 1.1"})
+        cls.equipment1_1 = cls.Equipment.create({"name": "Equipment 1.1"})
 
     def test_01_hierarchy(self):
         self.equipment1_1.parent_id = self.equipment1
