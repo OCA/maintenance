0636d19ca156b2679acbd8b35d1fa8cf36dcde96
Bhavesh Heliconia <oca@heliconia.in>
2025-01-24 20:54:32 +0530

    [IMP] maintenance_equipment_category_hierarchy: pre-commit auto fixes

--- maintenance_equipment_category_hierarchy/models/maintenance_equipment_category.py
+++ maintenance_equipment_category_hierarchy/models/maintenance_equipment_category.py
@@ -6,7 +6,6 @@ from odoo.exceptions import ValidationError
 
 
 class MaintenanceEquipmentCategory(models.Model):
-
     _inherit = "maintenance.equipment.category"
 
     _parent_name = "parent_id"
@@ -34,9 +33,8 @@ class MaintenanceEquipmentCategory(models.Model):
     def _compute_complete_name(self):
         for category in self:
             if category.parent_id:
-                category.complete_name = "{} / {}".format(
-                    category.parent_id.complete_name,
-                    category.name,
+                category.complete_name = (
+                    f"{category.parent_id.complete_name} / {category.name}"
                 )
             else:
                 category.complete_name = category.name
