f64f8b8872c91ba0931dd0a9d91f16773df8b390
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-07-27 12:56:54 +0200

    [OU-FIX] maintenance_timesheet: Pass env instead of cr
    Needed for 17+
    
    https://github.com/OCA/openupgradelib/blob/d2d76e772760365988a53af11b65eab648d991d2/openupgradelib/openupgrade.py#L333

--- maintenance_timesheet/migrations/18.0.1.1.0/post-migration.py
+++ maintenance_timesheet/migrations/18.0.1.1.0/post-migration.py
@@ -6,5 +6,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, "maintenance_timesheet", "migrations/18.0.1.1.0/noupdate_changes.xml"
+        env, "maintenance_timesheet", "migrations/18.0.1.1.0/noupdate_changes.xml"
     )
