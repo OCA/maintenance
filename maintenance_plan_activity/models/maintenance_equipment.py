a34feeeac03debc5ad24bde0a914df2cee71d440
Yann Papouin <ypa@decgroupe.com>
2025-10-20 16:30:39 +0200

    [IMP] maintenance_plan_activity: Prepare activity data

--- maintenance_plan_activity/models/maintenance_equipment.py
+++ maintenance_plan_activity/models/maintenance_equipment.py
@@ -9,6 +9,20 @@ from odoo import _, models
 class MaintenanceEquipment(models.Model):
     _inherit = "maintenance.equipment"
 
+    def _prepare_new_request_activity_values(self, request, activity):
+        """Prepare the values to create a new mail.activity for a maintenance request
+        created from a maintenance plan.
+        """
+        return {
+            "activity_type_id": activity.activity_type_id.id,
+            "note": _("Activity automatically generated from maintenance plan"),
+            "user_id": activity.user_id.id or self.env.user.id,
+            "res_id": request.id,
+            "res_model_id": self.env.ref("maintenance.model_maintenance_request").id,
+            "date_deadline": request.schedule_date
+            - timedelta(days=activity.date_before_request),
+        }
+
     def _create_new_request(self, maintenance_plan):
         new_requests = super()._create_new_request(maintenance_plan)
         for request in new_requests:
@@ -16,21 +30,11 @@ class MaintenanceEquipment(models.Model):
                 # In case mail_activity_team is installed this makes sure
                 # the correct activity team is selected. If that module is
                 # not installed the context does nothing
-                self.env["mail.activity"].with_context(
-                    default_res_model="maintenance.request"
-                ).create(
-                    {
-                        "activity_type_id": planned_activity.activity_type_id.id,
-                        "note": _(
-                            "Activity automatically generated from maintenance plan"
-                        ),
-                        "user_id": planned_activity.user_id.id or self.env.user.id,
-                        "res_id": request.id,
-                        "res_model_id": self.env.ref(
-                            "maintenance.model_maintenance_request"
-                        ).id,
-                        "date_deadline": request.schedule_date
-                        - timedelta(days=planned_activity.date_before_request),
-                    }
+                activity_data = self._prepare_new_request_activity_values(
+                    request, planned_activity
                 )
+                if activity_data:
+                    self.env["mail.activity"].with_context(
+                        default_res_model="maintenance.request"
+                    ).create(activity_data)
         return new_requests
