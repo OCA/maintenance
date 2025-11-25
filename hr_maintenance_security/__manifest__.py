669eeeff1b3812931cad3ca93639464f97ca7727
Víctor Martínez <victor.martinez@tecnativa.com>
2025-11-25 08:39:26 +0100

    [MIG] hr_maintenance_security: Migration to 18.0

--- hr_maintenance_security/__manifest__.py
+++ hr_maintenance_security/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "HR Maintenance Security",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/maintenance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
--- hr_maintenance_security/tests/test_maintenance_security.py
+++ hr_maintenance_security/tests/test_maintenance_security.py
@@ -1,16 +1,15 @@
 # Copyright 2023 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common, new_test_user
+from odoo.tests import new_test_user
 
-from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestMaintenanceSecurity(common.TransactionCase):
+class TestMaintenanceSecurity(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.hr_user = new_test_user(
             cls.env,
             login="test-maintenance-user",
