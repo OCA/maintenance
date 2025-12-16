be415acd09a7ef0eed0e191e0e081471bb867e5c
??? <dgallo@nuobit.com>
2025-12-16 12:04:46 +0100

    [MIG] maintenance_equipment_contract: Migration to 18.0

--- maintenance_equipment_contract/__manifest__.py
+++ maintenance_equipment_contract/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2019 Creu Blanca
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Maintenance Equipment Contract",
     "summary": """
         Manage equipment contracts""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
