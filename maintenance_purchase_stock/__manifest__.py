73be9553955ae1e05523ba38b13078e08c9e0e4d
christian-ramos-tecnativa <christian.ramos@tecnativa.com>
2025-12-31 15:31:15 +0000

    [ADD] maintenance_purchase_stock: New module

---
+++ maintenance_purchase_stock/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Tecnativa
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Maintenance Purchase Stock",
+    "summary": """
+        Create Equipments when recieving purchased products""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/maintenance",
+    "depends": [
+        "maintenance_purchase",
+        "purchase_stock",
+    ],
+    "data": [],
+    "demo": [],
+}
