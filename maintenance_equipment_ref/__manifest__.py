79dff47bd559747c77b635b01f370a381e1e4aea
Marcus Paulo Dib Lopes <marcuspaulodiblopes@gmail.com>
2026-07-24 13:36:33 -0300

    [MIG] maintenance_equipment_ref: Migration to 19.0

--- maintenance_equipment_ref/__manifest__.py
+++ maintenance_equipment_ref/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Maintenance Equipment Reference",
     "summary": "Adds reference field to maintenance equipment",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
