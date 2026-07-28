fd7f2d18cf93664a06770f6792e8d4b5c46459f2
Clément Rivière <criviere@teicee.com>
2026-07-28 12:20:41 +0200

    [MIG] maintenance_equipment_status: Migration to 19.0

--- maintenance_equipment_status/__manifest__.py
+++ maintenance_equipment_status/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Maintenance Equipment Status",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/maintenance",
